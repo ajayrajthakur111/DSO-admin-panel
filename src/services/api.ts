0ecf483960622793de283ac0ed7b4f761908e9b5
Ajay Rajput <ajayrajthakur111@gmail.com>
2025-05-03 19:15:00 +0530

    vite bug fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create a base axios instance
 export const api = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}`|| 'http://127.0.0.1:8000/',
+    baseURL: `${import.meta.env.VITE_APP_API_URL}`|| 'http://127.0.0.1:8000/',
     timeout: 10000,
     withCredentials: true,
     headers: {
