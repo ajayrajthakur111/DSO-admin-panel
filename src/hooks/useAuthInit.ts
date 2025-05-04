c9de981ffc1c8ffb5c47efc7c29f8b3df3f930e7
Ajay Rajput <ajayrajthakur111@gmail.com>
2025-05-04 10:57:55 +0530

    login bug fix

--- src/hooks/useAuthInit.ts
+++ src/hooks/useAuthInit.ts
@@ -1,14 +1,18 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../app/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../app/store';
 import { getCurrentUser } from '../features/auth/authSlice';
 import { getCustomers } from '../features/customers/customerSlice';
 
 export const useAuthInit = () => {
-  const dispatch = useDispatch<AppDispatch>();
+    const dispatch = useDispatch<AppDispatch>();
+    const initialized = useSelector((state: RootState) => state.auth.initialized);
 
-  useEffect(() => {
-    dispatch(getCurrentUser());
-    dispatch(getCustomers());
-  }, [dispatch]);
+    useEffect(() => {
+        if (!initialized) {
+            dispatch(getCurrentUser()).unwrap().then(() => {
+                dispatch(getCustomers());
+            });
+        }
+    }, [initialized, dispatch]);
 };
